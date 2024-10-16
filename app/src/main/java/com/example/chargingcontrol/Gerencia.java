63bca6699d632205325e7091b617c53f7c8f74f7
Cleilton-Santos <cleilton14s@gmail.com>
2024-10-16 10:32:01 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/chargingcontrol/Gerencia.java
@@ -0,0 +1,22 @@
+package com.example.chargingcontrol;
+
+import android.view.View;
+
+public class Gerencia {
+    //Definir um painel adiministrativo para visualizacao e controle geral
+    public void cleilton(){
+
+    }
+    public void fmmix(){
+
+        int[] Betoneiras = {2,3,4,5,6,7,8,9,10,11,12,13,21,25,26,27,28,39,48,51,53,93,96,101,140,141,142,143,144,149};
+        int[] Bombistas = {30,31,32,33,37,147,148,278};
+
+        void Motoristas(){
+
+        }
+    }
+    public void terraplanagem(){
+
+    }
+}
---
+++ app/src/main/java/com/example/chargingcontrol/Usina.java
@@ -0,0 +1,11 @@
+package com.example.chargingcontrol;
+
+public class Usina {
+    //Definir o fluxo de trabalho dos caminhoes dentro da usina, desde a chegada ate a saida
+    public static Entrada(){
+
+    }
+    public static Saida(){
+
+    }
+}
